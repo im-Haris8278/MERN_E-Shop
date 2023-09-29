b75ff9b8640a23abb9b7fc84e7815a7794ea5767
Haris Baig <harisbaig7081@gmail.com>
2023-09-29 20:33:39 +0500

    Fixed Cors

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "https://mern-e-shop-kucz.vercel.app/",
+    origin: "https://mern-e-shop-mlhy.vercel.app/",
     credentials: true,
   })
 );
